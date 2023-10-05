b356cefb64555d8b349878a039c3339826cf8c16
madbronco <69767445+madbronco@users.noreply.github.com>
2023-10-05 22:06:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -204,4 +204,4 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
         await msg.message.delete()
 
 async def pm_spoll_choker(msg):
-    await msg.reply("ദയവായി ഗ്രൂപ്പ് വഴി സെർച്ച് ചെയ്യുക. @pschelpergroup")
+    await msg.reply("ദയവായി ഗ്രൂപ്പ് വഴി സെർച്ച് ചെയ്യുക.\n\n@pschelpergroup")
